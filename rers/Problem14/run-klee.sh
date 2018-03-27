8d537d46126bfc6bde999e977fb844226d8bfdce
Bart van Oort <bvanoort@chello.nl>
2018-03-27 13:07:18 +0200

    Adds problem 14 with KLEE

---
+++ rers/Problem14/run-klee.sh
@@ -0,0 +1,10 @@
+echo -e "-------------------------------"
+echo -e "This script should be run inside the KLEE Docker container."
+echo -e "-------------------------------"
+echo -e "Compiling Problem14-KLEE.c for KLEE..."
+echo -e "-------------------------------"
+clang -I /home/klee/klee_src/include -emit-llvm -g -c Problem14-KLEE.c -o Problem14-KLEE.bc
+echo -e "-------------------------------"
+echo -e "Compiled. Running KLEE..."
+echo -e "-------------------------------"
+klee Problem14-KLEE.bc
