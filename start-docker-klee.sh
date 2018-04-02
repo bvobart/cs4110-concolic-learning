a9bbb25c80fc46ef76ffe94d390c29e01f703c09
Bart van Oort <bvanoort@chello.nl>
2018-04-02 23:00:51 +0200

    Renamed rers folder to klee

--- start-docker-klee.sh
+++ start-docker-klee.sh
@@ -5,5 +5,5 @@ sudo docker pull klee/klee
 echo -e "\n-------------------------------"
 echo -e "Running KLEE Docker image. rers folder is mounted to '~/rers'"
 echo -e "-------------------------------\n"
-sudo docker run -ti --rm --mount type=bind,source="$(pwd)"/rers,target=/home/klee/rers klee/klee
+sudo docker run -ti --rm --mount type=bind,source="$(pwd)"/klee,target=/home/klee/rers klee/klee
 echo -e "Bye!"
